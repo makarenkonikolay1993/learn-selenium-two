8af99d1163ecc7513efcdd324370239aace36395
makarenkonikolay1993 <makarenkonikolay1993@gmail.com>
2020-06-19 17:34:13 +0300

    Initial commit

---
+++ src/test/java/TestAutomation.java
@@ -0,0 +1,28 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestAutomation {
+    private WebDriver driver;
+    @BeforeClass
+    public void setUpDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
